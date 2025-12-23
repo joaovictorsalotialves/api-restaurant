3da585259808035223133cc742f8d5f1ecd58bca
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-23 12:10:48 -0300

    Listando produtos

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -6,7 +6,14 @@ import { knex } from "@/database/knex"
 class ProductsController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.json({ message: "ok" })
+      const { name } = request.query
+
+      const products = await knex<ProductRepository>("products")
+        .select()
+        .whereLike("name", `%${name ?? ""}%`)
+        .orderBy("name")
+
+      return response.json({ products })
     } catch (error) {
       next(error)
     }
