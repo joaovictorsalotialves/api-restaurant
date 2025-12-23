a65402ffde6cc43da76814b99a2c3bfea6f6b2a5
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-23 18:38:25 -0300

    Removendo produtos

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express"
 import z from "zod"
 
 import { knex } from "@/database/knex"
+import { AppError } from "@/utils/AppError"
 
 class ProductsController {
   async index(request: Request, response: Response, next: NextFunction) {
@@ -13,7 +14,7 @@ class ProductsController {
         .whereLike("name", `%${name ?? ""}%`)
         .orderBy("name")
 
-      return response.json({ products })
+      return response.json(products)
     } catch (error) {
       next(error)
     }
@@ -48,6 +49,15 @@ class ProductsController {
         .refine((value) => !isNaN(value), { message: "id must be a number" })
         .parse(request.params.id)
 
+      const product = await knex<ProductRepository>("products")
+        .select()
+        .where({ id })
+        .first()
+
+      if (!product) {
+        throw new AppError("product not found", 404)
+      }
+
       const bodySchema = z.object({
         name: z
           .string({ required_error: "name is required!" })
@@ -62,6 +72,32 @@ class ProductsController {
 
       await knex<ProductRepository>("products")
         .update({ name, price, updated_at: new Date() })
+        .where({ id })
+
+      return response.json()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async remove(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z.string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(request.params.id)
+
+      const product = await knex<ProductRepository>("products")
+        .select()
+        .where({ id })
+        .first()
+
+      if (!product) {
+        throw new AppError("product not found", 404)
+      }
+
+      await knex<ProductRepository>("products")
+        .delete()
         .where({ id })
 
       return response.json()
