8ac1133ab0ff73ddb136b84bff2feb8f49ac6f3a
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-23 23:01:29 -0300

    Regras de negócio do pedido

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from "express"
 import { z } from "zod"
 
+import { knex } from "@/database/knex"
+import { AppError } from "@/utils/AppError"
+
 class OrdersController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
@@ -11,6 +14,26 @@ class OrdersController {
       })
 
       const { table_session_id, product_id, quantity } = bodySchema.parse(request.body)
+
+      const session = await knex<TableSessionRepository>("tables_sessions")
+        .where({ id: table_session_id })
+        .first()
+
+      if (!session) {
+        throw new AppError("session table not found", 404)
+      }
+
+      if (session.closed_at) {
+        throw new AppError("this session table is already closed")
+      }
+
+      const product = await knex<ProductRepository>("products")
+        .where({ id: product_id })
+        .first()
+
+      if (!product) {
+        throw new AppError("product not found", 400)
+      }
 
       return response.status(201).json()
     } catch (error) {
